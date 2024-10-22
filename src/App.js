a4256c97af5caca514890f32d107fb00b92b2b17
Your Name <you@example.com>
2024-10-22 11:53:07 +0300

    Настройка проекта

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+const Div = styled.div`
+    text-align: center;
+`;
+export const App = () => {
+    return (
+        <Div>
+            <i className="fa fa-camera-retro"></i>
+            <div>Hello</div>
+        </Div>
+    );
+};
