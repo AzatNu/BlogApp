5fd4e03dd041a037022f435a00d9b1fc4710cc64
Your Name <you@example.com>
2024-10-24 14:37:55 +0300

    Реализованы маршруты, а так же сделаны наброски шапки,футера и главной страницы

---
+++ src/blog.js
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+const Content = styled.div`
+    text-align: center;
+    padding: 120px 0;
+`;
+const H2 = styled.h2`
+    text-align: center;
+`;
+const Header = () => <div>это шапка</div>;
+const Footer = () => <div>это подвал</div>;
+export const Blog = () => {
+    return (
+        <>
+            <Header />
+            <Content>
+                <H2>Контент</H2>
+                <Routes>
+                    <Route path="/" element={<div> Главная страница</div>} />
+                    <Route path="/login" element={<div>Авторизация</div>} />
+                    <Route path="/register" element={<div> Регистрация</div>} />
+                    <Route path="/users" element={<div>Пользователи</div>} />
+                    <Route path="/post" element={<div>Новая статья</div>} />
+                    <Route path="/post/:postId" element={<div>Статья</div>} />
+                    <Route path="*" element={<div>Ошибка</div>} />
+                </Routes>
+            </Content>
+            <Footer />
+        </>
+    );
+};
